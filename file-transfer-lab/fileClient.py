da4be30b12f726e73a2dac010117727af6cb0dc6
student <solomond10@yahoo.com>
2020-10-03 15:45:01 -0600

    Zero length files, files that don't exist and files that are already on the server are handled

--- file-transfer-lab/fileClient.py
+++ file-transfer-lab/fileClient.py
@@ -5,25 +5,30 @@
 import os, socket, re, sys
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect(('', 50001))
+clientSocket.connect(("127.0.0.1", 50001))
 
-#nameOfFile = "file.txt"
-#sizeOfFile = os.path.getsize(nameOfFile)
-
-fileNm = "file.txt"
+separator = ":"
+fileName = input("Enter the name of the file: ")
 
 try:
-    nameOfFile = open(fileNm, "r")
-    sizeOfFile = os.path.getsize(fileNm)
 
+    sizeOfFile = os.path.getsize(fileName)
+
+    if sizeOfFile == 0:
+        print("There's nothing in the file")
+    
+    clientSocket.send(f"{fileName}{separator}{sizeOfFile}".encode())
+    
+    with open (fileName, "rb") as a:
+        for data in fileName:
+            data = a.read(1024)
+
+            if data == "":
+                break
+
+            clientSocket.send(data)
+            
 except FileNotFoundError as e:
+
     print("The file doesn't exist")
 
-if sizeOfFile == 0:
-    print("There's nothing in the file")
-    #break
-
-for line in nameOfFile:
-    #print(line)
-    clientSocket.send((line).encode())
-    
--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -2,8 +2,7 @@
 
 #Server
 
-import socket, sys, re
-#from sockHelpers import sendAll
+import socket, sys, re, os
 
 ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ls.bind(('',50001))
@@ -11,6 +10,30 @@ ls.listen(1)
 
 convSock, clientAddr = ls.accept()
 
-content = convSock.recv(100)
+fileInfo = convSock.recv(1024).decode()
+fileName, fileSize = fileInfo.split(":")
 
-print(content)
+#Differentiates file name so we can see what's inside of the file that was recieved
+fileName = fileName.replace(".txt", "-received.txt")
+
+#checks to see if file already exist
+path = os.getcwd()+"/"+fileName
+
+if os.path.exists(path) is True:
+    print("The file is already on the server")
+    sys.exit(1)
+
+fileSize = int(fileSize)
+
+count = 0
+
+with open (fileName, "wb") as a:
+        while count < fileSize:
+            data = convSock.recv(1024)
+
+            if data == "":
+                break
+            
+            a.write(data)
+            count += 1
+
